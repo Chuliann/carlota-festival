b730c28af068825abe9ac8011f1f6cab2b9544f1
Julian Berton <julianbertonb@gmail.com>
2022-09-18 02:26:13 -0300

    Agregado barra para volver

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,49 +1,128 @@
 import logo from "../img/logo.png";
 import cerrar from "../img/cerrar.svg";
-import { useState } from "react";
+import slider from "../img/slide.png";
+import { useEffect, useState } from "react";
 import "../styles/header.css";
 
 const Header = () => {
-  const [expandido, setExpandido] = useState(false);
+    const [expandido, setExpandido] = useState(false);
+    const [navOculta, setNavOculta] = useState(false);
+    const [comportamiento, setComportamiento] = useState(false);
 
-  const handleMenu = () => {
-    setExpandido(!expandido);
-    document.querySelector("body").classList.toggle("blur-a");
-  };
+    const handleMenu = () => {
+        setExpandido(!expandido);
+        document.querySelector("body").classList.toggle("blur-a");
+    };
 
-  const handleNav = (valor) => {
-    console.log(valor)
-  }
+    const handleNav = (valor) => {
+        handleMenu();
+        document
+            .querySelector(`#${valor}`)
+            .scrollIntoView({ behavior: "smooth" });
+    };
 
-  return (
-    <header id="inicio">
-      <div className="idiomas">
-        <button className="boton-idioma">ESPAÑOL</button>
-        <button className="boton-idioma">ENGLISH</button>
-        <button className="boton-idioma">DEUTSCH</button>
-      </div>
-      <div className="navbar">
-        <img className="logo" alt="" src={logo}></img>
-        <button onClick={() => handleMenu()} className="boton-hamburguesa">
-          <span className="line1_hamburger"></span>
-          <span className="line2_hamburger"></span>
-          <span className="line3_hamburger"></span>
-        </button>
-        <button onClick={() => handleMenu()} className={`cross ${expandido ? "expandido" : ""}`}>
-          <img src={cerrar}></img>
-        </button>
-        <nav className={`nav-items ${expandido ? "expandido" : ""}`}>
-          <button className="link" value="inicio" onClick={(e) => handleNav(e.target.value)}>Inicio</button>
-          <button className="link" value="festival" onClick={(e) => handleNav(e.target.value)}>Festival</button>
-          <button className="link" value="inscribirse" onClick={(e) => handleNav(e.target.value)}>Inscribirse</button>
-          <button className="link" value="bases" onClick={(e) => handleNav(e.target.value)}>Bases</button>
-          <button className="link" value="nosotros" onClick={(e) => handleNav(e.target.value)}>Nosotros</button>
-        </nav>
-      </div>
+    useEffect(() => {
+        let lastScroll = window.scrollY;
+        const handleScroll = (event) => {
+            if (lastScroll > 500) {
+                setComportamiento(true);
+                if (lastScroll < window.scrollY) {
+                    setNavOculta(true);
+                } else {
+                    setNavOculta(false);
+                }
+            } else {
+                setNavOculta(false);
+                setComportamiento(false);
+            }
+            lastScroll = window.scrollY;
+        };
 
-      <div id="overlay" onClick={() => handleMenu()}></div>
-    </header>
-  );
+        window.addEventListener("scroll", handleScroll);
+
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
+
+    return (
+        <header id="inicio">
+            <div className="idiomas">
+                <button className="boton-idioma">ESPAÑOL</button>
+                <button className="boton-idioma">ENGLISH</button>
+                <button className="boton-idioma">DEUTSCH</button>
+            </div>
+            <nav
+                className={`navbar ${navOculta ? "nav-oculta" : ""} ${
+                    comportamiento ? "navarriba" : ""
+                }`}
+            >
+                <img className="logo" alt="" src={logo}></img>
+                <button
+                    onClick={() => handleMenu()}
+                    className="boton-hamburguesa"
+                >
+                    <span className="line1_hamburger"></span>
+                    <span className="line2_hamburger"></span>
+                    <span className="line3_hamburger"></span>
+                </button>
+                <button
+                    onClick={() => handleMenu()}
+                    className={`cross ${expandido ? "expandido" : ""}`}
+                >
+                    <img src={cerrar}></img>
+                </button>
+                <div className={`nav-items ${expandido ? "expandido" : ""}`}>
+                    <button
+                        className="link"
+                        value="inicio"
+                        onClick={(e) => handleNav(e.target.value)}
+                    >
+                        Inicio
+                    </button>
+                    <button
+                        className="link"
+                        value="festival"
+                        onClick={(e) => handleNav(e.target.value)}
+                    >
+                        Festival
+                    </button>
+                    <button
+                        className="link"
+                        value="inscribirse"
+                        onClick={(e) => handleNav(e.target.value)}
+                    >
+                        Inscribirse
+                    </button>
+                    <button
+                        className="link"
+                        value="bases"
+                        onClick={(e) => handleNav(e.target.value)}
+                    >
+                        Bases
+                    </button>
+                    <button
+                        className="link"
+                        value="nosotros"
+                        onClick={(e) => handleNav(e.target.value)}
+                    >
+                        Nosotros
+                    </button>
+                    <div className="idiomas-nav">
+                        <button>ES</button>
+                        <button>EN</button>
+                        <button>GN</button>
+                    </div>
+                </div>
+            </nav>
+
+            <div id="overlay" onClick={() => handleMenu()}></div>
+            <button 
+              onClick={() => document.querySelector('#inicio').scrollIntoView({ behavior: "smooth" })} className={`arrow-up ${!navOculta && comportamiento ? "comportamiento" : ""}`}>
+                <img src={slider} alt="up-arrow"></img>
+            </button>
+        </header>
+    );
 };
 
 export default Header;
