29092b6150a8c13036be34f32822db3d59e52d91
Julian Berton <julianbertonb@gmail.com>
2022-10-18 14:29:41 -0300

    comiteando

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -42,7 +42,7 @@ const Form = ({idioma}) => {
         }
 
         try {
-            await fetch("http://localhost/api2/index.php", {
+            await fetch("http://localhost/api3/index.php", {
             method: "POST",
             body: JSON.stringify(formulario),
             headers: {
