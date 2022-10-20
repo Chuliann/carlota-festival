169f43ae4533bd2b0537426db7d68a99329f6902
Julian Berton <julianbertonb@gmail.com>
2022-10-20 14:54:24 -0300

    ahora si

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -42,7 +42,7 @@ const Form = ({idioma}) => {
         }
 
         try {
-            await fetch("http://127.0.0.1/api7/index.php", {
+            await fetch("http://0.0.0.0/api7/index.php", {
             method: "POST",
             body: JSON.stringify(formulario),
             headers: {
